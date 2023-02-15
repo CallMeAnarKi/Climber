31681c18a8fc5b31c8a5e9db31c4153e00268140
CallMeAnarKi <randomless81@gmail.com>
2023-02-15 15:54:12 +0100

    Gamemanager now create the level. Poblate piece now set correctly the barriers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,24 @@ public class GameManager : MonoBehaviour
     private static GameManager _gameManager;
     private static GameObject player;
 
+    //barrier 
+    [SerializeField] private GameObject barrierGO;
+    public static GameObject[] barrierArray;
+    [SerializeField] private GameObject barrierParent;
+
+    //piece
+    [SerializeField] private GameObject pieceGO;
+    static public GameObject[] pieceArray;
+    [SerializeField] private GameObject pieceParent;
+    static private GameObject lastPieceSet;
+
     public enum Difficulty{ VeryEasy, Easy, Normal, Hard, VeryHard};
     private static Difficulty levelDifficulty;
     private static int playerScore = 0;
     private static float minRandomY;
     private static float maxRandomY;
-    private static float minRandomX;
-    private static float maxRandomX;
-
-
+    private static float widthBetweenBarriers;
+    
     private void Awake()
     {
         if (_gameManager ==  null)
@@ -27,9 +36,61 @@ public class GameManager : MonoBehaviour
         {
             player = GameObject.Find("Player");
         }
+
+
     }
 
-    static public (float, float, float, float) GenerateDifficulty()
+    private void Start()
+    {
+        //Move this function to scene management
+        CreateLevel();
+    }
+
+    //Call this function when Scene management implemented
+    private void CreateLevel()
+    {
+        barrierArray = new GameObject[50];
+        for (int i = 0; i < barrierArray.Length; i++)
+        {
+            barrierArray[i] = Instantiate(barrierGO, new Vector2(1000, 1000), Quaternion.identity, barrierParent.transform);
+            barrierArray[i].SetActive(false);
+        }
+
+        pieceArray = new GameObject[4];
+        for (int i = 0; i < pieceArray.Length; i++)
+        {
+            pieceArray[i] = Instantiate(pieceGO, new Vector2(2000, 2000), Quaternion.identity, pieceParent.transform);
+            pieceArray[i].SetActive(false);
+        }
+
+        for (int i = 0; i < pieceArray.Length-1; i++)
+        {
+            if (!pieceArray[i].activeSelf)
+            {                
+                if (i != 0)
+                {
+                    pieceArray[i].transform.position = new Vector2(0, lastPieceSet.transform.position.y + lastPieceSet.GetComponentInChildren<RectTransform>().rect.height);
+                }
+                else
+                {
+                    pieceArray[i].transform.position = new Vector2(0, 0);
+                }
+
+                lastPieceSet = pieceArray[i];
+
+                pieceArray[i].SetActive(true);
+                pieceArray[i].GetComponentInChildren<PoblatePiece>().SetBarriers(pieceArray[i].GetComponent<Transform>());                
+            }
+        }
+    }
+
+    static public void NextPiece()
+    {
+
+    }
+
+
+    static public (float, float, float) GenerateDifficulty()
     {
         if (playerScore < 500)
         {
@@ -44,8 +105,8 @@ public class GameManager : MonoBehaviour
         {
             case Difficulty.VeryEasy:
 
-                minRandomX = 1;
-                maxRandomX = 3;
+                //how many times is bigger the gap than the player between barriers 
+                widthBetweenBarriers = 1.2f;
 
                 minRandomY = 3;
                 maxRandomY = 8;
@@ -62,7 +123,7 @@ public class GameManager : MonoBehaviour
                 break;
         }
 
-        return (minRandomX, maxRandomX, minRandomY, maxRandomY);
+        return (widthBetweenBarriers, minRandomY, maxRandomY);
     }
     
     static public GameObject GetPlayer()
--- Assets/Scripts/PoblatePiece.cs
+++ Assets/Scripts/PoblatePiece.cs
@@ -5,65 +5,91 @@ using UnityEngine;
 public class PoblatePiece : MonoBehaviour
 {
     [SerializeField] private GameObject barrier;
-    [SerializeField] private RectTransform pieceRectTransform;
+    [SerializeField] private RectTransform pieceRT;
+
+    private RectTransform playerRef;
     
     private GameObject previousBarrier;
     private RectTransform barrierRT;
-    private float minRandom;
-    private float maxRandom;
+    private float widthBetweenBarriers;
+    private float minRandomY;
+    private float maxRandomY;
     private float randomY;
     private float randomX;
-
-
-
+    private bool lastBarrierPlaced;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         barrierRT = (RectTransform)barrier.transform;
+        playerRef = GameManager.GetPlayer().GetComponent<RectTransform>();
 
-        if (previousBarrier == null)
-        {
-            previousBarrier = Instantiate(barrier, new Vector2(0, pieceRectTransform.rect.yMin + barrierRT.rect.yMax), Quaternion.identity);
-        }
-
-        SetDifficulty();
-        StartCoroutine(SpawnBarrier(minRandom, maxRandom));
-
-
+        
+        //StartCoroutine(SpawnBarrier(widthBetweenBarriers, minRandomY,maxRandomY));
     }
 
-    private IEnumerator SpawnBarrier(float min, float max)
+    private void SpawnBarrier(Transform pieceObjective,float widthBetweenBarriers, float minY, float maxY)
     {
-        while (previousBarrier.transform.position.y < pieceRectTransform.rect.yMax)
+        while (previousBarrier.transform.position.y < pieceObjective.position.y + pieceRT.rect.yMax && !lastBarrierPlaced)
         {
-            randomY = Random.Range(min, max);
-            randomX = Random.Range(barrierRT.rect.xMax, max);
+            randomX = Random.Range(pieceRT.rect.xMin - barrierRT.rect.xMax, pieceRT.rect.xMax - barrierRT.rect.xMax - (playerRef.rect.width * widthBetweenBarriers));
+            randomY = Random.Range(minY, maxY);
 
+            for (int i = 0; i < GameManager.barrierArray.Length; i++)
+            {
+                if (!GameManager.barrierArray[i].activeSelf)
+                {                    
+                    GameManager.barrierArray[i].transform.position = new Vector2(randomX, 
+                                                                                 previousBarrier.transform.position.y + randomY);
+                    previousBarrier = GameManager.barrierArray[i];
+                    previousBarrier.SetActive(true);
 
-            previousBarrier = Instantiate(barrier, 
-                                        new Vector2(
-                                            pieceRectTransform.rect.xMin - randomX, //hacer k las barreras se posicionen a diferentes X 
-                                            previousBarrier.transform.position.y + randomY + barrierRT.rect.yMax), 
-                                        Quaternion.identity);
+                    GameManager.barrierArray[i + 1].transform.position = new Vector2(previousBarrier.transform.position.x + barrierRT.rect.width + (playerRef.rect.width * widthBetweenBarriers),
+                                                                                     previousBarrier.transform.position.y);
+                    GameManager.barrierArray[i + 1].SetActive(true);
 
-
-
-            yield return new WaitForSeconds(0);
+                    //if the last pair of barriers are upper than max.y of the piece, they are deactivated
+                    if (previousBarrier.transform.position.y > pieceObjective.position.y + pieceRT.rect.yMax)
+                    {
+                        previousBarrier.SetActive(false);
+                        GameManager.barrierArray[i + 1].SetActive(false);
+                        lastBarrierPlaced = !lastBarrierPlaced;
+                    }
+                    break;
+                }
+            }       
         }
 
-        yield return null;
+        return ;
     }
 
     private void SetDifficulty()
     {
-        minRandom = GameManager.GenerateDifficulty().Item1;
-        maxRandom = GameManager.GenerateDifficulty().Item2;
+        widthBetweenBarriers = GameManager.GenerateDifficulty().Item1;
+        minRandomY = GameManager.GenerateDifficulty().Item2;
+        maxRandomY = GameManager.GenerateDifficulty().Item3;  
     }
 
-
-
+    public void SetBarriers(Transform pieceObjective)
+    {
+        if (previousBarrier == null)
+        {
+            for (int i = 0; i < GameManager.barrierArray.Length; i++)
+            {
+                if (!GameManager.barrierArray[i].activeSelf)
+                {
+                    previousBarrier = GameManager.barrierArray[i];
+                    //previousBarrier.transform.position = new Vector2(pieceRT.rect.center.x, pieceRT.rect.min.y + barrierRT.rect.max.y);
+                    previousBarrier.transform.position = new Vector2(pieceObjective.position.x, pieceObjective.position.y - pieceRT.rect.yMax + barrierRT.rect.max.y);
+                    previousBarrier.SetActive(true);
+                    break;
+                }
+            }
+        }
+        SetDifficulty();
+        SpawnBarrier(pieceObjective, widthBetweenBarriers, minRandomY, maxRandomY);
+    }
 
 
 }
